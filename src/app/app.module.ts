03ed33736d5d768594837970269772b9d310848c
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-12-21 21:50:35 +0100

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -10,6 +10,7 @@ import { PriceComponent } from './components/price/price.component';
 import { ActionsComponent } from './components/actions/actions.component';
 import { CartComponent } from './components/cart/cart.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,13 @@ import { LogoComponent } from './components/logo/logo.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
